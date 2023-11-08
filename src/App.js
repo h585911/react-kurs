54c19c249cef9fe29b73062e3e615ebf78f56cff
h585911 <585911@stud.hvl.no>
2023-11-08 17:37:18 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Header from "./components/Header";
 import { Stack } from "@mui/material";
 import Description from "./components/Description";
 import ImageComponent from "./components/ImageComponent";
+import DailyQuote from "./components/DailyQuote";
 
 export default function App() {
   return (
     <Container maxWidth="lg">
       <Header />
       <Stack direction="column" spacing={2} mt={2}>
+        <DailyQuote />
         <Description />
         <ImageComponent />
       </Stack>
